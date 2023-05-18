0ce054794828cf0bec4629fb67cc5def394943a1
Danil Markov <markov.danil.04@mail.ru>
2023-05-18 13:40:31 +0300

    Работа с несколькими активностями
    Signed-off-by: Danil Markov <markov.danil.04@mail.ru>

---
+++ SixApplication/app/src/main/java/com/example/sixapp/Main.java
@@ -0,0 +1,27 @@
+package com.example.sixapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ViewUtils;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_act);
+    }
+
+    public void openSecondForm(View view){
+        Intent intent = new Intent(this, Second.class);
+        startActivity(intent);
+    }
+
+    public void openThirdForm(View view){
+        Intent intent = new Intent(this, Third.class);
+        startActivity(intent);
+    }
+}
